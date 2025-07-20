7e3c039b52f3219cadc1eb32473b107c32c76c90
Sonal Wijenayaka <sonalwijenayaka@gmail.com>
2025-07-20 11:24:12 +0530

    Update StudentTable.js

--- demo-app/src/StudentTable.js
+++ demo-app/src/StudentTable.js
@@ -2,6 +2,14 @@ export default function StudentTable(){
     return (
         <div className="container">
              <h2>Student Records</h2>
+             <div className="table-container">
+                <a href="#" class="btn btn-add">Add new Student</a>
+                <table>
+                    <thead>
+                        
+                    </thead>
+                </table>
+             </div>
         </div>
     )
 }
