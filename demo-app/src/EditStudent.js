c3d4623048ce42c887175585e5a7cf5378f918ba
Sonal Wijenayaka <sonalwijenayaka@gmail.com>
2025-07-23 08:37:14 +0530

    view students

--- demo-app/src/EditStudent.js
+++ demo-app/src/EditStudent.js
@@ -1,7 +1,77 @@
+import { Link } from "react-router-dom";
+
 export default function EditStudent(){
+      const { studentid } = useParams();
+ // const [studentData, setStudentData] = useState(null); // define state
+
+  useEffect(() => {
+    fetch("http://localhost:8000/students/" + studentid)
+      .then((res) => res.json())
+      .then((data) => setStudentData(data)) // store data in state
+      .catch((err) => console.log(err.message));
+  }, [studentid]);
+    const handleSubmit=()=>{
+
+    }
     return (
         <div className="container">
-             <h2>Editing Student Details </h2>
+               <div className="form-box">
+      <h2>Edit Student Details</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="id">Id:</label>
+        <input
+          type="text"
+          id="id"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+        />
+        {submitted && id.length === 0 && (
+          <span className="errorMsg">Please enter your ID</span>
+        )}
+
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        {submitted && name.length === 0 && (
+          <span className="errorMsg">Please enter your name</span>
+        )}
+
+        <label htmlFor="place">Place:</label>
+        <input
+          type="text"
+          id="place"
+          value={place}
+          onChange={(e) => setPlace(e.target.value)}
+        />
+        {submitted && place.length === 0 && (
+          <span className="errorMsg">Please enter your place</span>
+        )}
+
+        <label htmlFor="phone">Phone:</label>
+        <input
+          type="text"
+          id="phone"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
+        {submitted && phone.length === 0 && (
+          <span className="errorMsg">Please enter your phone number</span>
+        )}
+
+        <div>
+          <button type="submit" className="btn btn-add">
+            Save
+          </button>
+          <Link to="/" className="btn btn-danger">
+            Back
+          </Link>
+        </div>
+      </form>
+    </div>
              
         </div>
     )
--- demo-app/src/ViewDetails.js
+++ demo-app/src/ViewDetails.js
@@ -1,14 +1,19 @@
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams} from "react-router-dom";
 
 export default function ViewDetails() {
   const { studentid } = useParams();
-  const [studentData, setStudentData] = useState(null); // define state
-
+ const [studentData, setStudentData] = useState(null); // define state
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [place, setPlace] = useState("");
+  const [Phone, setPhone] = useState("");
+  const [submitted, setSubmitted] = useState(false); // Tracks if form has been submitted
+  const navigate = useNavigate();
   useEffect(() => {
     fetch("http://localhost:8000/students/" + studentid)
       .then((res) => res.json())
-      .then((data) => setStudentData(data)) // store data in state
+      .then((data) => console.log(data)) 
       .catch((err) => console.log(err.message));
   }, [studentid]);
 
